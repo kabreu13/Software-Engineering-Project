8265c64e3746f0dbbbdad3cb1ab10b1c4701cc8d
kabreu <38060048+kabreu13@users.noreply.github.com>
2023-01-31 15:36:10 -0500

    Update Test.java
    DON'T RUN THIS

--- Test.java
+++ Test.java
@@ -1,5 +1,15 @@
 public class Test {
     public static void main(String[] args) {
+
+        for(int i = 0; i < 10; i++){
+            for(int i2 = 0; i2 < 10; i2++){
+                for(int i3 = 0; i3 < 10; i3++){
+                    for(int i4 = 0; i4 < 10; i4++){
+                        System.out.println("Hello, World!");
+                    }
+                }
+            }
+        }
         System.out.println("Hello, world!");
         System.out.println("Test");
         System.out.println("Test");
